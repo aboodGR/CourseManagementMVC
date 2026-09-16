a3d156c8196dffd40945dbfbcad30e74df570223
AboodGR <abood22446699@gmail.com>
2026-09-16 15:43:17 +0300

    Complete Course Management MVC project

---
+++ WebApplication1/Controllers/CoursesController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CoursesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var courses = _context.Courses.ToList();
+            return View(courses);
+        }
+
+        public IActionResult Details(int id) {
+            var course = _context.Courses.FirstOrDefault(c => c.Id == id);
+            if (course == null) {
+                return NotFound();
+            }
+            return View(course);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Course course) {
+            _context.Courses.Add(course);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id) {
+            var course = _context.Courses.FirstOrDefault(c => c.Id == id);
+            if (course == null) {
+                return NotFound();
+            }
+            return View(course);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Course course) {
+            _context.Courses.Update(course);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var course = _context.Courses.FirstOrDefault(c => c.Id == id);
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View(course);
+        }
+        [HttpPost]
+        public IActionResult Delete(Course course)
+        {
+            _context.Courses.Remove(course);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ WebApplication1/Models/Course.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Teacher { get; set; }
+        public decimal Price { get; set; }
+
+
+
+    }
+}
